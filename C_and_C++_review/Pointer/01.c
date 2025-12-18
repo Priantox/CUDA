73b809ec36a474a8454790af92b219a46568773c
Prianto <priantox07@gmail.com>
2025-12-18 10:54:35 +0600

    few changes

--- C_and_C++_review/Pointer/01.c
+++ C_and_C++_review/Pointer/01.c
@@ -6,5 +6,14 @@ int main(){
     int **ptr2 =  &ptr1;
     int ***ptr3 = &ptr2;
 
+    int num = 20;
+    float fnum = 3.12;
+    void* vptr = &num;
+
     printf("Value: %d\n", ***ptr3);
+
+    printf("Integar: %d\n", *(int*)vptr);
+
+    vptr = &fnum;
+    printf("Float: 0.2f\n", *(float*)vptr);
 } 
